4023792e359da211a711ca34117ecf32682cf06c
thanhbinh89 <ntbinh.work@gmail.com>
2024-10-22 09:43:10 +0000

    add cross compile

---
+++ compile.sh
@@ -0,0 +1,10 @@
+
+export CC=/home/ubuntu/luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-gcc
+export CXX=/home/ubuntu/luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-g++
+export STRIP=/home/ubuntu/luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-strip
+export AR=/home/ubuntu/luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-ar
+export RANLIB=/home/ubuntu/luckfox-pico/tools/linux/toolchain/arm-rockchip830-linux-uclibcgnueabihf/bin/arm-rockchip830-linux-uclibcgnueabihf-ranlib
+
+./configure --host=arm-rockchip830-linux-uclibcgnueabihf --enable-static --disable-shared
+
+make -j6
